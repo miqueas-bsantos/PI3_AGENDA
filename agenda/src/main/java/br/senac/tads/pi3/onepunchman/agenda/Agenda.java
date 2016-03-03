7d0b3b9a112563b1a4143b8431cad8d0a3efd6d6
miqueas.bsantos1 <miqueas.bsantos1@CAS419657W07G07.senacsp.edu.br>
2016-03-03 19:10:13 -0300

    Cadastrar

--- agenda/src/main/java/br/senac/tads/pi3/onepunchman/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/onepunchman/agenda/Agenda.java
@@ -4,6 +4,19 @@
  * and open the template in the editor.
  */
 package br.senac.tads.pi3.onepunchman.agenda;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 /**
  *
@@ -11,4 +24,105 @@ package br.senac.tads.pi3.onepunchman.agenda;
  */
 public class Agenda {
     
+   private Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        // Passo 1: Registrar driver JDBC.
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
+
+        // Passo 2: Abrir a conexÃ£o
+        conn = DriverManager.getConnection(
+                "jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app", // usuario
+                "app"); // senha
+        return conn;
+    }
+
+    public void listarPessoas() {
+        Statement stmt = null;
+        Connection conn = null;
+
+        String sql = "SELECT ID_CONTATO, NM_CONTATO, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL FROM TB_CONTATO";
+        try {
+            conn = obterConexao();
+            stmt = conn.createStatement();
+            ResultSet resultados = stmt.executeQuery(sql);
+
+            DateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+
+            while (resultados.next()) {
+                Long id = resultados.getLong("ID_CONTATO");
+                String nome = resultados.getString("NM_CONTATO");
+                Date dataNasc = resultados.getDate("DT_NASCIMENTO");
+                String email = resultados.getString("VL_EMAIL");
+                String telefone = resultados.getString("VL_TELEFONE");
+                System.out.println(String.valueOf(id) + ", " + nome + ", " + formatadorData.format(dataNasc) + ", " + email + ", " + telefone);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    public void cadastrarPessoas(String nome, String d, String telefone, String email) {
+        PreparedStatement prestmt = null;
+        Connection conn = null;
+
+        String sql = "INSERT INTO TB_CONTATO(NM_CONTATO, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL, DT_CADASTRO)"
+                + "VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)";
+        
+        try {
+            conn = obterConexao();
+            prestmt = conn.prepareStatement(sql);
+   
+            java.util.Date myDate = new java.util.Date(d);
+            java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+            
+            prestmt.setString(1, nome);
+            prestmt.setDate(2, sqlDate);
+            prestmt.setString(3, telefone);
+            prestmt.setString(4, email);
+            prestmt.executeUpdate();
+            prestmt.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (prestmt != null) {
+                try {
+                    prestmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+
+
 }
